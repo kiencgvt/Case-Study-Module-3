b930ef30f17839010bd41e10712e0b582ae1b09f
Doan Quan <NE.qkvt@gmail.com>
2021-07-16 16:20:21 +0700

    tao giao dien admin

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('/admin')->group(function (){
+    Route::get('/home', function (){
+        return view('admin.home');
+    });
 });
