e033e197bf38b766217481b9ecc432851f2ed097
locvankhoic1020g1 <locvankhoi@gmail.com>
2021-07-16 16:54:51 +0700

    create_database

---
+++ database/migrations/2021_07_16_082938_create_foods_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('image');
+            $table->string('name');
+            $table->integer('price');
+            $table->unsignedInteger('promotion_price');
+            $table->unsignedInteger('service_charge');
+            $table->unsignedInteger('shop_id');
+            $table->unsignedInteger('category_id');
+            $table->date('require_date');
+            $table->string('tag');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
---
+++ database/migrations/2021_07_16_094347_connect_forein_key_from_order_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ConnectForeinKeyFromOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->foreign('food_id')->references('id')->on('foods');
+            $table->foreign('order_id')->references('id')->on('order');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            //
+        });
+    }
+}
