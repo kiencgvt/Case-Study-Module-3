e8fb67225e045d061d7dd44d314e291dad74f77a
Ninh02 <ninhdo10021998@gmail.com>
2021-07-18 14:15:21 +0700

    update_collaborator

--- resources/views/collaborators/layouts/master.blade.php
+++ resources/views/collaborators/layouts/master.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Askbootstrap">
     <meta name="author" content="Askbootstrap">
     <link rel="icon" type="image/png" href="img/fav.png">
-    <title>Swiggiweb - Online Food Ordering Website Template</title>
+    <title> Swiggiweb - Online Food Ordering Website Template</title>
     <!-- Slick Slider -->
     <link rel="stylesheet" type="text/css" href="{{asset('css/slick.min.css')}}" />
     <link rel="stylesheet" type="text/css" href="{{asset('css/slick-theme.min.css')}}" />
@@ -151,7 +151,35 @@
     </div>
     <!-- profile -->
     <div class="container position-relative">
+        <div class="py-5 osahan-profile row">
+{{--            <div class="col-md-4 mb-3">--}}
+{{--                <div class="bg-white rounded shadow-sm sticky_sidebar overflow-hidden">--}}
+{{--                    <a href="profile.html" class="">--}}
+{{--                        <div class="d-flex align-items-center p-3">--}}
+{{--                            <div class="left mr-3">--}}
+{{--                                <img alt="#" src="img/user1.jpg" class="rounded-circle">--}}
+{{--                            </div>--}}
+{{--                            <div class="right">--}}
+{{--                                <h6 class="mb-1 font-weight-bold">Gurdeep Singh <i class="feather-check-circle text-success"></i></h6>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </a>--}}
+{{--                    <!-- profile-details -->--}}
+{{--                    <div class="bg-white profile-details">--}}
 
+{{--                        <a href="{{ route('collab.create') }}" class="d-flex w-100 align-items-center border-bottom px-3 py-4">--}}
+{{--                            <div class="left mr-3">--}}
+{{--                                <h6 class="font-weight-bold m-0 text-dark"> Thêm sản phẩm</h6>--}}
+{{--                            </div>--}}
+{{--                        </a>--}}
+{{--                        <a href="{{ route('collab.edit') }}" class="d-flex w-100 align-items-center px-3 py-4">--}}
+{{--                            <div class="left mr-3">--}}
+{{--                                <h6 class="font-weight-bold m-0 text-dark"></i>xoa san pham</h6>--}}
+{{--                            </div>--}}
+{{--                        </a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
             <div class="col-md-8 mb-3">
                 @yield('content')
             </div>
