28e9c77544193be1b8d79146f6d38e64691dbd3f
Doan Quan <NE.qkvt@gmail.com>
2021-07-19 21:03:07 +0700

    fix_restau

--- resources/views/front-end/restau.blade.php
+++ resources/views/front-end/restau.blade.php
@@ -308,13 +308,12 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 pt-3">
+            <div class="col-md-4">
                 <div class="osahan-cart-item rounded rounded shadow-sm overflow-hidden bg-white sticky_sidebar">
                     <div class="d-flex border-bottom osahan-cart-item-profile bg-white p-3">
                         <img alt="osahan" src="img/starter1.jpg" class="mr-3 rounded-circle img-fluid">
                         <div class="d-flex flex-column">
-                            <h6 class="mb-1 font-weight-bold">Spice Hut Indian Restaurant</h6>
-                            <p class="mb-0 small text-muted"><i class="feather-map-pin"></i> 2036 2ND AVE, NEW YORK, NY 10029</p>
+                            <h6 class="mb-1 font-weight-bold">NAME</h6>
                         </div>
                     </div>
                     <div class="bg-white border-bottom py-2">
@@ -382,22 +381,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="bg-white p-3 py-3 border-bottom clearfix">
-                        <div class="input-group-sm mb-2 input-group">
-                            <input placeholder="Enter promo code" type="text" class="form-control">
-                            <div class="input-group-append"><button type="button" class="btn btn-primary"><i class="feather-percent"></i> APPLY</button></div>
-                        </div>
-                        <div class="mb-0 input-group">
-                            <div class="input-group-prepend"><span class="input-group-text"><i class="feather-message-square"></i></span></div>
-                            <textarea placeholder="Any suggestions? We will pass it on..." aria-label="With textarea" class="form-control"></textarea>
-                        </div>
-                    </div>
                     <div class="bg-white p-3 clearfix border-bottom">
-                        <p class="mb-1">Item Total <span class="float-right text-dark">$3140</span></p>
-                        <p class="mb-1">Restaurant Charges <span class="float-right text-dark">$62.8</span></p>
-                        <p class="mb-1">Delivery Fee<span class="text-info ml-1"><i class="feather-info"></i></span><span class="float-right text-dark">$10</span></p>
-                        <p class="mb-1 text-success">Total Discount<span class="float-right text-success">$1884</span></p>
-                        <hr>
                         <h6 class="font-weight-bold mb-0">TO PAY <span class="float-right">$1329</span></h6>
                     </div>
                     <div class="p-3">
