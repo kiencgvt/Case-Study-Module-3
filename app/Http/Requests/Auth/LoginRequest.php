856998e55a4e0a35c2dae39263ca45b4a1c2f596
Doan Quan <NE.qkvt@gmail.com>
2021-07-19 14:26:24 +0700

    edit view login

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -46,6 +46,7 @@ class LoginRequest extends FormRequest
         $this->ensureIsNotRateLimited();
 
         if (! Auth::attempt($this->only('email', 'password', 'role'), $this->boolean('remember'))) {
+
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+
 
 Route::get('/', function () {
     return view('front-end.home');
 });
 Route::prefix('collab')->middleware('auth.collaborator')->group(function (){
-//    return view('collaborators.layouts.master');
     Route::get('/',[FoodController::class,'index'])->name('collab.index');
     Route::get('/create',[FoodController::class,'create'])->name('collab.create');
     Route::post('/create',[FoodController::class,'store'])->name('collab.store');
@@ -35,22 +32,33 @@ Route::prefix('collab')->middleware('auth.collaborator')->group(function (){
     Route::get('/search',[FoodController::class,'search'])->name('collab.search');
 });
 
-Route::prefix('user')->middleware('auth.admin')->group(function (){
-    Route::get('/',[UserController::class,'getAllUser'])->name('user.index');
-    Route::get('/search',[UserController::class,'searchUser'])->name('user.search');
-    Route::get('/{id}/delete',[UserController::class,'deleteUser'])->name('user.delete');
-});
-Route::prefix('category')->middleware('auth.admin')->group(function (){
 
-    Route::get('/',[CategoryController::class,'getAllCategory'])->name('category.list');
-    Route::get('/add',[CategoryController::class,'addCategory'])->name('category.add');
-    Route::post('/add',[CategoryController::class,'store'])->name('category.store');
-    Route::get('/{id}/edit',[CategoryController::class,'editCategory'])->name('category.edit');
-    Route::post('/{id}/edit',[CategoryController::class,'updateCategory'])->name('category.update');
-    Route::get('/{id}/delete',[CategoryController::class,'deleteCategory'])->name('category.delete');
-    Route::get('/search',[CategoryController::class,'searchCategory'])->name('category.search');
+Route::prefix('admin')->group(function (){
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::prefix('users')->middleware('auth.admin')->group(function (){
+        Route::get('/',[UserController::class,'getAllUser'])->name('user.index');
+        Route::get('/search',[UserController::class,'searchUser'])->name('user.search');
+        Route::get('/{id}/delete',[UserController::class,'deleteUser'])->name('user.delete');
+    });
+
+    Route::prefix('categories')->middleware('auth.admin')->group(function (){
+        Route::get('/',[CategoryController::class,'getAllCategory'])->name('category.list');
+        Route::get('/add',[CategoryController::class,'addCategory'])->name('category.add');
+        Route::post('/add',[CategoryController::class,'store'])->name('category.store');
+        Route::get('/{id}/edit',[CategoryController::class,'editCategory'])->name('category.edit');
+        Route::post('/{id}/edit',[CategoryController::class,'updateCategory'])->name('category.update');
+        Route::get('/{id}/delete',[CategoryController::class,'deleteCategory'])->name('category.delete');
+        Route::get('/search',[CategoryController::class,'searchCategory'])->name('category.search');
+    });
 });
+
+Route::get('login', [\App\Http\Controllers\AuthController::class,'showFormLogin'])->name('auth.shoFormLogin');
+
 Route::get('/cart',function (){
     return view('front-end.cart');
 });
-require __DIR__.'/auth.php';
+
