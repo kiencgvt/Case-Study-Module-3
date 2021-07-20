99044bd41c36407cdb9e307065762fc4c7012573
Ninh02 <ninhdo10021998@gmail.com>
2021-07-20 19:09:55 +0700

    create_view_detail

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Food;
 use App\Models\Shop;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RestaurantController extends Controller
 {
     function detail($id) {
         $restaurantCurrent = Shop::findOrFail($id);
-        return view('front-end.restaurants.detail', compact('restaurantCurrent'));
+        $foods = Food::where('shop_id',$id)->get();
+        $categories = Category::all();
+        return view('front-end.restaurants.detail', compact('restaurantCurrent','foods','categories'));
     }
 }
