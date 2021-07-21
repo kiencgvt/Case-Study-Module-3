b29f5fae1726f72c7b01761607d54380c933e257
locvankhoic1020g1 <locvankhoi@gmail.com>
2021-07-21 11:30:16 +0700

    sua giao dien trang edit category

--- resources/views/admin/foodCategory/list.blade.php
+++ resources/views/admin/foodCategory/list.blade.php
@@ -199,16 +199,7 @@
         }
 
     </style>
-    <script>
 
-        $(document).ready(function () {
-
-            $(document).ready(function () {
-
-                $('[data-toggle="tooltip"]').tooltip();
-
-            });
-    </script>
     <div class="">
         <div class="table-responsive">
             <div class="table-wrapper">
@@ -232,22 +223,18 @@
                         </ul>
                     </div>
                 @endif
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="{{route('category.add')}}">Add Category</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <form class="form-inline my-2 my-lg-0" action="{{route('category.search')}}" method="get">
+                <div class="row">
+                    <div class="col-4">
+                        <a class="btn btn-success" href="{{route('category.add')}}">Add Category</a>
+                    </div>
+                    <div class="col-8">
+                        <form style="float: right" class="form-inline my-2 my-lg-0" action="{{route('category.search')}}" method="get">
                             <input class="form-control mr-sm-2" name="search" type="search"
                                    placeholder="Search Category" aria-label="Search">
                             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                         </form>
                     </div>
-                </nav>
+                </div>
                 <table class="table table-striped table-hover" style="width: 40%">
 
                     <table class="table table-striped table-hover">
