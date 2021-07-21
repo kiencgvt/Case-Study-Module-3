b35147d1ee434f6ddb4ef8bd7d229f71c0227524
locvankhoic1020g1 <locvankhoi@gmail.com>
2021-07-21 10:34:03 +0700

    them cot image vao bang categories

--- resources/views/admin/foodCategory/add.blade.php
+++ resources/views/admin/foodCategory/add.blade.php
@@ -1,8 +1,12 @@
 @extends('admin.master')
 @section('title','Add Category')
 @section('content')
-    <form action="{{route('category.store')}}" method="post">
+    <form action="{{route('category.store')}}" method="post" enctype="multipart/form-data">
         @csrf
+        <div class="form-group">
+            <label for="image">Category Image</label>
+            <input type="file" class="form-control" name="image" id="image">
+        </div>
         <div class="form-group">
             <label for="exampleInputEmail1">Name</label>
             <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
